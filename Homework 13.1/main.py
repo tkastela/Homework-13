e16aba5adee9080de4418cee6cc619b95683e8f7
Tomislav Kaštela <101247343+tkastela@users.noreply.github.com>
2022-04-26 12:24:53 +0200

    Add files via upload

---
+++ Homework 13.1/main.py
@@ -0,0 +1,95 @@
+class Player:
+    def __init__(self, first_name, last_name, height_cm, weight_kg):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.height_cm = height_cm
+        self.weight_kg = weight_kg
+
+    def weight_to_lbs(self):
+        pounds = self.weight_kg * 2.20
+        return pounds
+
+
+class BasketballPlayer(Player):
+    def __init__(self, first_name, last_name, height_cm, weight_kg, points, rebounds, assists):
+        Player.__init__(self, first_name, last_name, height_cm, weight_kg)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.height_cm = height_cm
+        self.weight_kg = weight_kg
+        self.points = points
+        self.rebounds = rebounds
+        self.assists = assists
+
+
+class FootballPlayer(Player):
+    def __init__(self, first_name, last_name, height_cm, weight_kg, goals, yellow_cards, red_cards):
+        Player.__init__(self, first_name, last_name, height_cm, weight_kg)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.height_cm = height_cm
+        self.weight_kg = weight_kg
+        self.goals = goals
+        self.yellow_cards = yellow_cards
+        self.red_cards = red_cards
+
+
+lebron = BasketballPlayer(first_name="Lebron", last_name="James", height_cm=203, weight_kg=113, points=27.2,
+                          rebounds=7.4, assists=7.2)
+
+kev_dur = BasketballPlayer(first_name="Kevin", last_name="Durant", height_cm=210, weight_kg=108, points=27.2,
+                           rebounds=7.1, assists=4)
+print(lebron.first_name)
+print(lebron.height_cm)
+
+print(kev_dur.last_name)
+print(kev_dur.rebounds)
+
+bball_players = [lebron, kev_dur]
+
+for player in bball_players:
+    print(player.last_name + ", " + player.first_name)
+
+
+lebron_dict = {"first_name": "Lebron", "last_name": "James", "height_cm": 203, "weight_kg": 113, "points": 27.2,
+               "rebounds": 7.4, "assists": 7.2}
+
+print(lebron_dict["first_name"])
+print(lebron_dict["height_cm"])
+
+
+print(lebron.weight_to_lbs())
+print(kev_dur.weight_to_lbs())
+
+
+ronaldo = FootballPlayer(first_name="Cristiano", last_name="Ronaldo", height_cm=184, weight_kg=79, goals=586,
+                         yellow_cards=95, red_cards=11)
+
+messi = FootballPlayer(first_name="Lionel", last_name="Messi", height_cm=170, weight_kg=67, goals=575,
+                       yellow_cards=67, red_cards=0)
+
+print(ronaldo.first_name)
+print(ronaldo.goals)
+print(messi.first_name)
+print(messi.goals)
+print(messi.weight_to_lbs())
+
+
+print("Enter some football player's data!")
+
+f_name = input("Enter player's first name: ")
+l_name = input("Enter player's last name: ")
+height = input("Enter player's height: ")
+weight = input("Enter player's weight: ")
+goals = input("Enter the number of player's goals: ")
+y_cards = input("Enter the number of player's yellow cards: ")
+r_cards = input("Enter the number of player's red cards: ")
+
+new_player = FootballPlayer(first_name=f_name, last_name=l_name, height_cm=float(height), weight_kg=float(weight),
+                            goals=int(goals), yellow_cards=int(y_cards), red_cards=int(r_cards))
+
+with open("football_players.json", "w") as football_file:
+    football_file.write(str(new_player.__dict__))
+
+print("Player successfully entered!")
+print("Player's data: {0}".format(new_player.__dict__))
---
+++ homework 13.2/main.py
@@ -0,0 +1,67 @@
+import datetime
+import json
+import random
+
+
+class Result:
+    def __init__(self, attempts, player_name, date):
+        self.attempts = attempts
+        self.player_name = player_name
+        self.date = date
+
+
+def play_game():
+    secret = random.randint(1, 30)
+    attempts = 0
+    score_list = get_score_list()
+    player_name = input("Please enter your name: ")
+
+    while True:
+        guess = int(input("Guess the secret number (between 1 and 30): "))
+        attempts += 1
+
+        if guess == secret:
+            result_obj = Result(attempts=attempts, player_name=player_name, date=str(datetime.datetime.now()))
+
+            score_list.append(result_obj.__dict__)
+
+            with open("score_list.json", "w") as score_file:
+                score_file.write(json.dumps(score_list))
+
+            print("You've guessed it - congratulations! It's number " + str(secret))
+            print("Attempts needed: " + str(attempts))
+            break
+        elif guess > secret:
+            print("Your guess is not correct. Try something smaller...")
+        elif guess < secret:
+            print("Your guess is not correct. Try something bigger...")
+
+
+def get_score_list():
+    with open("score_list.json", "r") as score_file:
+        score_list = json.loads(score_file.read())
+        return score_list
+
+
+def get_top_scores():
+    score_list = get_score_list()
+    top_score_list = sorted(score_list, key=lambda k: k['attempts'])[:3]
+    return top_score_list
+
+
+while True:
+    selection = input("Would you like to A) Play a new game, B) See the best scores or C) Quit? ")
+
+    if selection.upper() == "A":
+        play_game()
+    elif selection.upper() == "B":
+        for score_dict in get_top_scores():
+            result_obj = Result(attempts=score_dict.get("attempts"),
+                                player_name=score_dict.get("player_name", "Anonymous"),
+                                date=score_dict.get("date"))
+
+            print("Player: {name}; Attempts: {attempts}; Date: {date}".format(name=result_obj.player_name,
+                                                                              attempts=result_obj.attempts,
+                                                                              date=result_obj.date))
+    else:
+        break
